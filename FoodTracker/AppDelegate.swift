b18769df4acd080e78204d7e9b192420d0a40424
Masaki Murakami <yvzbtbcug@gmail.com>
2019-10-20 11:28:38 +0900

    チュートリアルの完成

--- FoodTracker/AppDelegate.swift
+++ FoodTracker/AppDelegate.swift
@@ -2,9 +2,6 @@
 //  AppDelegate.swift
 //  FoodTracker
 //
-//  Created by Masaki Murakami on 2019/08/24.
-//  Copyright © 2019 Murakami Masaki. All rights reserved.
-//
 
 import UIKit
 import CoreData
